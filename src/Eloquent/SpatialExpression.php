59f4e3a7681ddf1f1cdfd4825e405d1d28f06ad4
Timothy Elias <elias@mogic.com>
2024-08-07 14:34:06 +0200

    Support MariaDB by removing 3rd parameter to ST_GeomFromText
    MySQL allows 3 parameters, MariaDB only two:
    - https://dev.mysql.com/doc/refman/8.4/en/gis-wkt-functions.html#function_st-geomfromtext
    - https://mariadb.com/kb/en/st_geomfromtext/
    
    Related: https://github.com/grimzy/laravel-mysql-spatial/pull/152
    Related: https://github.com/grimzy/laravel-mysql-spatial/issues/138

--- src/Eloquent/SpatialExpression.php
+++ src/Eloquent/SpatialExpression.php
@@ -9,7 +9,7 @@ class SpatialExpression extends Expression
 {
     public function getValue(Grammar $grammar)
     {
-        return "ST_GeomFromText(?, ?, 'axis-order=long-lat')";
+        return "ST_GeomFromText(?, ?)";
     }
 
     public function getSpatialValue()
